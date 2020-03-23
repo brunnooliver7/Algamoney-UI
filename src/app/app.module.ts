4b2436a29b2862377da13cce68c58e36b77ef865
Bruno <brunno.oliver7@gmail.com>
2020-03-23 20:58:59 -0300

    Aula 10.4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,11 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+// import { AppRoutingModule } from './app-routing.module';
+import {InputTextModule} from 'primeng/inputtext';
+import {ButtonModule} from 'primeng/button';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +14,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    InputTextModule,
+    ButtonModule
+    // AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
